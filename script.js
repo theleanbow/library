8c8a5de6d26ed24f2eb255baaa1f71521f9638d6
LeanBow <theleanbow@gmail.com>
2022-07-18 22:22:57 +0545

    add updating of the book details

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 let myLibrary = [];
+let bookDetails;
 let library = document.querySelector("h1");
 let form = document.querySelector("form");
 let addBook = document.querySelector("#addBook");
+function submit() {
+  let bookSubmit = document.querySelector(".bookSubmit");
+  bookSubmit.addEventListener("click", () => {
+    bookDetails = new Book(
+      form.title.value,
+      form.author.value,
+      parseInt(form.pages.value)
+    );
+    myLibrary.push(bookDetails);
+    form.reset();
+  });
+}
+// });
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
 addBook.addEventListener("click", () => {
   form.classList.remove("hidden");
+  addBook.classList.add("hidden");
 });
 
 // div = document.createElement("div");
 //   div.innerText = "hello";
 //   div.classList.add("card");
 //   library.appendChild(div);
-function Book(title, author, pages) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-}
-
-function addBookToLibrary() {
-  let title = prompt("enter book name");
-  let author = prompt("enter book author");
-  let pages = prompt("enter book pages");
-  let bookDetails = new Book(title, author, pages);
-  myLibrary.push(bookDetails);
-}
-function printBooks() {
-  console.table(myLibrary);
-}
+submit();
