9ee5fa9eaffe50581ae3e2defefbf50a24d67344
LeanBow <theleanbow@gmail.com>
2022-07-20 12:05:18 +0545

    add better popup, hide books when adding new one

--- script.js
+++ script.js
@@ -4,20 +4,28 @@ let container = document.querySelector(".div");
 let library = document.querySelector("h1");
 let form = document.querySelector("form");
 let addBook = document.querySelector("#addBook");
+let div = document.querySelectorAll(".div");
 function submit() {
   let bookSubmit = document.querySelector(".bookSubmit");
   bookSubmit.addEventListener("click", () => {
-    bookDetails = new Book(
-      form.title.value,
-      form.author.value,
-      parseInt(form.pages.value),
-      form.hasRead.value
-    );
-    myLibrary.push(bookDetails);
-    form.reset();
-    display();
-    form.classList.add("hidden");
-    addBook.classList.remove("hidden");
+    if (
+      form.title.value != "" &&
+      form.author.value != "" &&
+      form.pages.value != ""
+    ) {
+      bookDetails = new Book(
+        form.title.value,
+        form.author.value,
+        parseInt(form.pages.value),
+        form.hasRead.value
+      );
+      myLibrary.push(bookDetails);
+      form.reset();
+      display();
+      form.classList.toggle("hidden");
+      addBook.classList.toggle("hidden");
+      // div.classList.remove("hidden");
+    }
   });
 }
 function Book(title, author, pages, hasRead) {
@@ -27,9 +35,11 @@ function Book(title, author, pages, hasRead) {
   this.hasRead = hasRead;
 }
 addBook.addEventListener("click", () => {
-  form.classList.remove("hidden");
-  addBook.classList.add("hidden");
+  form.classList.toggle("hidden");
+  addBook.classList.toggle("hidden");
+  // div.classList.add("hidden");
 });
+
 function display() {
   for (let i = myLibrary.length - 1; i < myLibrary.length; i++) {
     div = document.createElement("div");
@@ -41,12 +51,16 @@ function display() {
     pagesCard.innerText = myLibrary[i].pages;
     hasReadCard = document.createElement("div");
     hasReadCard.innerText = myLibrary[i].hasRead;
+    removeBook = document.createElement("button");
+    removeBook.innerText = "Remove";
+    removeBook.classList.add("removeButtonCSS");
     container.appendChild(div);
     div.classList.add("card");
     div.appendChild(titleCard);
     div.appendChild(authorCard);
     div.appendChild(pagesCard);
     div.appendChild(hasReadCard);
+    div.appendChild(removeBook);
   }
 }
 submit();
